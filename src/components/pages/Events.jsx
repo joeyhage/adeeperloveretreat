e7143fa5ab4cb53fc108a39ca1dbc20743b0835b
Glitch (adeeperloveretreat) <none>
2019-01-21 00:16:54 +0000

    UI tweaks and wording changes

--- src/components/pages/Events.jsx
+++ src/components/pages/Events.jsx
@@ -13,19 +13,19 @@ export default class Events extends React.Component {
           <img className='is-background' src='https://cdn.glitch.com/b8ab5d72-f094-4be6-b779-eb4ed16e6d67%2FDeeperLoveRetreat.JPG?1543365322759' alt='A Deeper Love Retreat' />
         </section>
         <section className='section' style={{marginRight:'15vw', marginLeft:'15vw'}}>       
-            <p className='title has-text-justified is-size-4-desktop is-size-6-touch'>
+            <p className='title is-size-4-desktop is-size-6-touch'>
               With busy and chaotic lives, we often forget to focus on our marriages. Whether you 
               are newly married or have been married a long time, we would love for you and your spouse
               to leave behind the distractions of everyday life and come to the tranquility
               of the Weber Farm for the day and focus on the beauty God gave us in nature and 
               within our marriages. 
             </p>
-            <p className='title has-text-justified is-size-4-desktop is-size-6-touch'>
+            <p className='title is-size-4-desktop is-size-6-touch'>
               Due to the new venue and wanting to maintain the peacefulness of the farm, we 
               will not have babysitting provided this year. However, if you need assistance in 
               finding a reliable sitter, you may contact us for suggestions. 
             </p>
-            <p className='title has-text-justified is-size-4-desktop is-size-6-touch'>
+            <p className='title is-size-4-desktop is-size-6-touch'>
               Please stay tuned for further details and a confirmed date. We look forward
               to helping you find A Deeper Love in your marriage!
             </p>
--- src/components/pages/Testimonials.jsx
+++ src/components/pages/Testimonials.jsx
@@ -18,28 +18,28 @@ export default class Testimonials extends React.Component {
               <div className='tile'>
                 <div className='tile is-parent is-vertical'>
                   <article className='tile is-child notification' style={{backgroundColor:'#8a94a9'}}>
-                    <p className='subtitle has-text-justified' style={{color:'#FFF'}}><FontAwesomeIcon icon={faQuoteLeft} alt='Quote Left' /> For the first time in 4 years of marriage my husband & I chose to go on a marriage retreat. 
+                    <p className='subtitle' style={{color:'#FFF'}}><FontAwesomeIcon icon={faQuoteLeft} alt='Quote Left' />&nbsp;For the first time in 4 years of marriage my husband & I chose to go on a marriage retreat. 
                     This retreat was life changing for us & taught us the importance of making this an annual occurance.  
                     We enjoyed the time to go out on a date during the retreat, the daycare provided by the planning committee, 
                     & the ability to connect with other married couples with small children.  Some of my favorite parts of the day 
                     were listening to the variety of speakers, the music, but then also the potluck dinner at the end of the day.  
                     For a couple that both work & have a little one, we appreciated a retreat that we could do for just the day.  
-                    It made it very manageable, but yet a great experience. <FontAwesomeIcon icon={faQuoteRight} alt='Quote Right' /></p>
-                    <p className='subtitle has-text-justified is-italic is-uppercase'style={{color:'#FFF'}}> -Breanne and Sam K. </p>
+                    It made it very manageable, but yet a great experience.&nbsp;<FontAwesomeIcon icon={faQuoteRight} alt='Quote Right' /></p>
+                    <p className='subtitle is-italic is-uppercase'style={{color:'#FFF'}}> -Breanne and Sam K.</p>
                   </article>
                   <article className='tile is-child notification'style={{backgroundColor:'#6d7993'}}>
-                    <p className='subtitle' style={{color:'#FFF'}}><FontAwesomeIcon icon={faQuoteLeft} alt='Quote Left' /> We enjoyed taking some time for to focus on just us and not the needs
+                    <p className='subtitle' style={{color:'#FFF'}}><FontAwesomeIcon icon={faQuoteLeft} alt='Quote Left' />&nbsp;We enjoyed taking some time for to focus on just us and not the needs
                       of the kids for a day. It was also great to be surrounded by so many other faithful couples. 
-                      What a relaxing day to reconnect!<FontAwesomeIcon icon={faQuoteRight} alt='Quote Right' /> </p>
-                      <p className='subtitle has-text-justified is-italic is-uppercase'style={{color:'#FFF'}}> -Kayla and Kyle </p>
+                      What a relaxing day to reconnect!&nbsp;<FontAwesomeIcon icon={faQuoteRight} alt='Quote Right' /> </p>
+                      <p className='subtitle is-italic is-uppercase'style={{color:'#FFF'}}> -Kayla and Kyle </p>
                   </article>
                   <article className='tile is-child notification'style={{backgroundColor:'#576176'}}>
                     
-                    <p className='subtitle'style={{color:'#FFF'}}><FontAwesomeIcon icon={faQuoteLeft} alt='Quote Left' /> My husband and I felt spoiled by a whole day to nourish our relationship! 
+                    <p className='subtitle'style={{color:'#FFF'}}><FontAwesomeIcon icon={faQuoteLeft} alt='Quote Left' />&nbsp;My husband and I felt spoiled by a whole day to nourish our relationship! 
                       The marriage retreat provided fun fellowship with other couples, refreshing prayer, inspirational talks, and a long 
                       lunch date to share our hearts with each other in regards to everything we were experiencing. Since we've been back 
-                      in the daily grind, I've been motivated to love better by looking back at moments that struck me from the retreat. <FontAwesomeIcon icon={faQuoteRight} alt='Quote Right' /></p>
-                    <p className='subtitle has-text-justified is-italic is-uppercase'style={{color:'#FFF'}}> -Tessa and Roberto </p>
+                      in the daily grind, I've been motivated to love better by looking back at moments that struck me from the retreat.&nbsp;<FontAwesomeIcon icon={faQuoteRight} alt='Quote Right' /></p>
+                    <p className='subtitle is-italic is-uppercase'style={{color:'#FFF'}}> -Tessa and Roberto</p>
                   </article>
                 </div>
                 <div className='tile is-parent'>
